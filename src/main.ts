12acf405a20ee2b02c57bbb122dd7f6373b1c427
Joao-Abner <joaoabnerpereira@gmail.com>
2025-09-26 18:40:46 -0300

    feat: add class-transformer and class-validator dependencies; implement global validation pipe in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Converte automaticamente os tipos (ex: string para number)
+      whitelist: true, // Remove propriedades não definidas no DTO
+      forbidNonWhitelisted: true, // Lança um erro se propriedades não definidas forem encontradas
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
