c02fc2e380e53f1bb376fdf11918c936aa1cccfb
Joao-Abner <joaoabnerpereira@gmail.com>
2025-09-26 20:09:29 -0300

    fix: add missing numeroPatrimonio field to Patrimonio interface

--- src/patrimonios/interfaces/patrimonio.interface.ts
+++ src/patrimonios/interfaces/patrimonio.interface.ts
@@ -1,6 +1,7 @@
 export interface Patrimonio {
   id: string;
   nome: string;
+  numeroPatrimonio: number; // número do patrimônio usado como código de barras
   descricao: string;
   valor: number;
   localizacao: string;
