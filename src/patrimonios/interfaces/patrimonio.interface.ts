b526b79205cd2e0d1de5203a814acefad993b4f8
Joao-Abner <joaoabnerpereira@gmail.com>
2025-09-26 21:07:13 -0300

    fix: update localizacao field type in Patrimonio interface to use LocalizacaoDto

--- src/patrimonios/interfaces/patrimonio.interface.ts
+++ src/patrimonios/interfaces/patrimonio.interface.ts
@@ -1,10 +1,11 @@
+import { LocalizacaoDto } from '../dto/localizacao.dto';
 export interface Patrimonio {
   id: string;
   nome: string;
   numeroPatrimonio: number; // número do patrimônio usado como código de barras
   descricao: string;
   valor: number;
-  localizacao: string;
+  localizacao: LocalizacaoDto; // Alterado de string para o nosso DTO
   observacao?: string;
   responsavel: string;
   status: 'ativo' | 'inativo' | 'manutencao';
