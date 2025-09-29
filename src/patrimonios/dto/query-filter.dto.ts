4f6552e53d6b2c9c0cc675bbf7c71c5a4f984dd9
Joao-Abner <joaoabnerpereira@gmail.com>
2025-09-29 12:37:43 -0300

    feat: add custom validation messages to query-filterDto

--- src/patrimonios/dto/query-filter.dto.ts
+++ src/patrimonios/dto/query-filter.dto.ts
@@ -4,20 +4,20 @@ import { Transform } from 'class-transformer';
 export class QueryFilterDto {
   @IsOptional()
   @Transform(({ value }) => parseInt(value, 10))
-  @IsNumber()
+  @IsNumber({}, { message: 'A página deve ser um número válido.' })
   page?: number;
 
   @IsOptional()
   @Transform(({ value }) => parseInt(value, 10))
-  @IsNumber()
+  @IsNumber({}, { message: 'O limite deve ser um número válido.' })
   limit?: number;
 
   @IsOptional()
-  @IsIn(['ativo', 'inativo', 'manutencao'])
+  @IsIn(['ativo', 'inativo', 'manutencao'], { message: "O status deve ser 'ativo', 'inativo' ou 'manutencao'." })
   status?: 'ativo' | 'inativo' | 'manutencao';
 
   @IsOptional()
-  @IsString()
+  @IsString({ message: 'O filtro deve ser uma string válida.' })
   @Transform(({ value }) => value.trim())
-  filtro?: string; // Adicionando o filtro de busca por nome
+  filter?: string; // Adicionando o filtro de busca por nome
 }
