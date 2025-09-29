5b4d8afcdde7bd7ef87a31706ec286cb3b85ddb1
Joao-Abner <joaoabnerpereira@gmail.com>
2025-09-29 12:31:44 -0300

    feat: add custom validation messages to LocalizacaoDto

--- src/patrimonios/dto/localizacao.dto.ts
+++ src/patrimonios/dto/localizacao.dto.ts
@@ -1,15 +1,15 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LocalizacaoDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O bloco deve ser uma string válida.' })
+  @IsNotEmpty({ message: 'O bloco é obrigatório.' })
   bloco: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O piso deve ser uma string válida.' })
+  @IsNotEmpty({ message: 'O piso é obrigatório.' })
   piso: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'A sala deve ser uma string válida.' })
+  @IsNotEmpty({ message: 'A sala é obrigatória.' })
   sala: string;
 }
