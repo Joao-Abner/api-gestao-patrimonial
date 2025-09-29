4adcda771dbade498ede8f6f18426ba1a917e93e
Joao-Abner <joaoabnerpereira@gmail.com>
2025-09-29 12:30:06 -0300

    feat: add custom validation messages to CreatePatrimonioDto

--- src/patrimonios/dto/create-patrimonio.dto.ts
+++ src/patrimonios/dto/create-patrimonio.dto.ts
@@ -10,37 +10,37 @@ import { Type } from 'class-transformer';
 import { LocalizacaoDto } from '../dto/localizacao.dto';
 
 export class CreatePatrimonioDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O nome deve ser uma string válida.' })
+  @IsNotEmpty({ message: 'O nome é obrigatório.' })
   nome: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'A descrição deve ser uma string válida.' })
+  @IsNotEmpty({ message: 'A descrição é obrigatória.' })
   descricao: string;
 
-  @IsNumber()
-  @IsNotEmpty()
+  @IsNumber({}, { message: 'O número do patrimônio deve ser numérico.' })
+  @IsNotEmpty({ message: 'O número do patrimônio é obrigatório.' })
   numeroPatrimonio: number;
 
-  @IsNumber()
-  @IsNotEmpty()
+  @IsNumber({}, { message: 'O valor deve ser numérico.' })
+  @IsNotEmpty({ message: 'O valor é obrigatório.' })
   valor: number;
 
-  @IsNotEmpty()
-  @ValidateNested() // Diz ao class-validator para validar o objeto aninhado
+  @IsNotEmpty({ message: 'A localização é obrigatória.' })
+  @ValidateNested({ message: 'A localização deve ser um objeto válido.' }) // Diz ao class-validator para validar o objeto aninhado
   @Type(() => LocalizacaoDto) // Diz ao class-transformer como criar uma instância de LocalizacaoDto
   localizacao: LocalizacaoDto;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O responsável deve ser uma string válida.' })
+  @IsNotEmpty({ message: 'O responsável é obrigatório.' })
   responsavel: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @IsIn(['ativo', 'inativo', 'manutencao'])
+  @IsString({ message: 'O status deve ser uma string válida.' })
+  @IsNotEmpty({ message: 'O status é obrigatório.' })
+  @IsIn(['ativo', 'inativo', 'manutencao'], { message: "O status deve ser 'ativo', 'inativo' ou 'manutencao'." })
   status: 'ativo' | 'inativo' | 'manutencao';
 
-  @IsString()
+  @IsString({ message: 'As observações devem ser uma string válida.' })
   @IsOptional()
   observacoes?: string;
 }
