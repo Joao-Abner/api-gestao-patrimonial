43368658c7ece95896333ca5141b826a8811de41
Joao-Abner <joaoabnerpereira@gmail.com>
2025-10-06 15:19:31 -0300

    feat: add filter custom-exception

---
+++ src/custom-exception/custom-exception.filter.ts
@@ -0,0 +1,22 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class CustomExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response: Response = ctx.getResponse();
+    const status = exception.getStatus();
+
+    response.status(status).json({
+      statusCode: status,
+      message: exception.message || 'Erro interno no servidor',
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
