ceb292d3ad2321fe8473d6796d140dbc2b4ebd9f
Joao-Abner <joaoabnerpereira@gmail.com>
2025-10-22 19:41:51 -0300

    refactor: rename class UserService to UsersServices.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
 @Injectable()
-export class UserService {
+export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async create(data: Prisma.UserCreateInput) {
