5a2610c3d5eb30e51f431af4a55909ee5ae35b7b
Joao-Abner <joaoabnerpereira@gmail.com>
2025-10-27 20:55:54 -0300

    feat: Import PassportModule and JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy'; // (será criado em breve)
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'minha_chave_secreta',
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy, PrismaService],
+  exports: [AuthService],
 })
 export class AuthModule {}
