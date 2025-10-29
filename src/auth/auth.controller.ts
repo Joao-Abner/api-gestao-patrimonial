41fc7a870250f33126045f3b2a2f6c7495d7976c
Joao-Abner <joaoabnerpereira@gmail.com>
2025-10-29 11:59:52 -0300

    feat: Creating the AuthController (Routes for Registration and Login)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED) // por padrão Post retorna 201 Createds
+  async register(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Body('name') name?: string,
+  ) {
+    const newUser = await this.authService.register(email, password, name);
+    return newUser;
+  }
+  @Post('login')
+  @HttpCode(HttpStatus.OK) // define o status como 200 OK explicitamente
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const jwt = await this.authService.login(email, password);
+    return jwt; // retornará { access_token: '...' }
+  }
+}
