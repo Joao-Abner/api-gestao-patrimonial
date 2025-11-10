acb319e3e3d4129f5a5ffc61423fa545ff8e9871
Joao-Abner <joaoabnerpereira@gmail.com>
2025-11-10 16:18:14 -0300

    feat: Add Swagger configuration to main.ts
    This commit adds the Swagger configuration to the main.ts file, allowing automatic API documentation using Swagger. The Swagger configuration includes the title, description, version, and JWT authentication.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Patrimonios API com Swagger')
+    .setDescription('API para gerenciamento de patrimônios')
+    .setVersion('1.0')
+    .addBearerAuth() // para habilitar autenticação via token JWT
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // Converte automaticamente os tipos (ex: string para number)
