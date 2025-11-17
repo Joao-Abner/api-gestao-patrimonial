a90dd381818ffdc62c8790207c3a7292e8540a11
Joao-Abner <joaoabnerpereira@gmail.com>
2025-11-17 11:55:42 -0300

    refactor: Removed commented out email property and example in LoginDto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,9 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
-  // @ApiProperty({ example: 'joao@email.com' })
-  // email: string;
-
   @ApiProperty({
     description: 'Email de login do usuário',
     example: 'usuario@email.com',
@@ -20,6 +17,4 @@ export class LoginDto {
   @IsString({ message: 'A senha deve ser uma string.' })
   @IsNotEmpty({ message: 'A senha é obrigatória.' })
   password: string;
-  // @ApiProperty({ example: '12345678' })
-  // password: string;
 }
