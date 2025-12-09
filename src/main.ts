46f10a3364136a65e8400355b91f1123e48f08c4
Joao-Abner <joaoabnerpereira@gmail.com>
2025-12-09 19:48:11 -0300

    feat: enable global exception filter in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './errors/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.useGlobalFilters(new HttpExceptionFilter()); // Filtro Global de Erros (ID5)
 
   app.useGlobalPipes(
     new ValidationPipe({
